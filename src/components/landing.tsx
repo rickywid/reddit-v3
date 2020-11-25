e5d83a866db82cc9c75c03bd7cf751f245c69bd7
rickywid <rickywid@hotmail.com>
2020-11-25 02:51:15 -0500

    disabled fb button

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -11,7 +11,7 @@ const Landing = () => {
             <p>A quick and easy way to keep up to date with the subreddits that's important to you.</p>
             <div className="login-btn-group">
                 <button className="btn-google" onClick={() => window.open(`${process.env.REACT_APP_SERVER}/api/auth/google`, "_self")}><Google />Log In with Google</button>
-                <button className="btn-facebook" onClick={() => window.open(`${process.env.REACT_APP_SERVER}/api/auth/facebook`, "_self")}><Facebook />Log In with Facebook</button>
+                <button className="btn-facebook" onClick={() => window.open(`${process.env.REACT_APP_SERVER}/api/auth/facebook`, "_self")} disabled><Facebook />Facebook (coming soon)</button>
                 <button className="btn-twitter" onClick={() => window.open(`${process.env.REACT_APP_SERVER}/api/auth/twitter`, "_self")}><Twitter />Log In with Twitter</button>
                 <button className="btn-github" onClick={() => window.open(`${process.env.REACT_APP_SERVER}/api/auth/github`, "_self")}><Github />Log In with Github</button>
             </div>
