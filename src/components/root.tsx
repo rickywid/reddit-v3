ce3a2f9dcf03340c7d49ffe21509a240f420895b
rickywid <rickywid@hotmail.com>
2020-12-04 17:53:28 -0500

    fix lint warnings

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -4,6 +4,7 @@ import Dash from './dash';
 import UserContext from '../context/userContext';
 
 const Root = () => {
+    // eslint-disable-next-line
     const {user, setUser} = useContext(UserContext);
     return (
         <div style={{height: '90vh', display: 'flex'}}>
