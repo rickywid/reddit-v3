0e9c171200643917abec41a50d72be73921fec1a
rickywid <rickywid@hotmail.com>
2021-01-26 20:42:41 -0500

    update env vars

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,2 +1,3 @@
 export const redditURL = "https://www.reddit.com"
-export const domain = "https://codeconcept.io"
+// export const domain = "https://codeconcept.io"
+export const domain = "http://localhost:5000"
