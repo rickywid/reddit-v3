7c57d0a0473e3080f355cc89c7baef8c0af8c73d
rickywid <rickywid@hotmail.com>
2021-01-26 22:00:34 -0500

    update signin login issue info

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -7,7 +7,7 @@ import { ReactComponent as Google } from '../assets/google.svg';
 
 const Landing = () => {
     return (
-        <div style={{margin: 'auto'}}>
+        <div style={{ margin: 'auto' }}>
             <h1>Personalized Reddit</h1>
             <p>A quick and easy way to keep up to date with the subreddits that's important to you.</p>
             <div className="login-btn-group">
@@ -16,7 +16,15 @@ const Landing = () => {
                 <button className="btn-twitter" onClick={() => window.open(`${process.env.REACT_APP_SERVER}/api/auth/twitter`, "_self")}><Twitter />Log In with Twitter</button>
                 <button className="btn-github" onClick={() => window.open(`${process.env.REACT_APP_SERVER}/api/auth/github`, "_self")}><Github />Log In with Github</button>
             </div>
-            <small style={{marginTop: '10px', display: 'inline-block'}}><i>*if you have trouble signing in, disable any ad blockers.</i></small>
+            <small style={{ marginTop: '50px', marginBottom:'10px', display: 'inline-block' }}><i>*Trouble signing in? Try the following:</i></small>
+            <ul>
+                <li>
+                    <small style={{ display: 'block ' }}><i>- If using Chrome browser, you may have to <a style={{ color: 'blue' }} href="https://support.siteimprove.com/hc/en-gb/articles/360007364778-Turning-off-Google-Chrome-SameSite-Cookie-Enforcement">disable default Cookie settings</a>.</i></small>
+                </li>
+                <li>
+                    <small style={{ display: 'block ' }}><i>- Try another browser such as Firefox, Brave, Internet Explorer.</i></small>
+                </li>
+            </ul>
         </div>
     )
 }
